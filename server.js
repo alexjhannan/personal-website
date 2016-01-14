00a387180a9c5cdcf3e9909bbaec84840dea7c03
dasbuuten <alexjhannan@gmail.com>
2016-01-14 10:46:49 -0500

    first commit. basic portfolio redirects to code examples.

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+// CONFIG
+app.use(express.static('client'));
+
+// ROUTES
+app.get('/', function(req, res){
+	res.sendFile(__dirname + '/client/index.html');
+});
+
+var server = app.listen('3000', function(){
+	var port = server.address().port;
+	console.log('Listening on port %s', port);
+});
