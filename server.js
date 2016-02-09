0db170919ab992be8b11bff88183c25c101bfb36
alexjhannan <alexjhannan@gmail.com>
2016-02-09 08:46:29 -0500

    added error handling routes

--- server.js
+++ server.js
@@ -51,6 +51,35 @@ app.post('/contactform', bodyParser.urlencoded({ extended: true }), function(req
 	
 });
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler. will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// live error handler, no stacktrace
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+ 	});
+ });
+
 var server = app.listen(process.env.PORT || 3000, function(){
 	var port = server.address().port;
 	console.log('Listening on port %s', port);
