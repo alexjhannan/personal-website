cec160ea36233995c76fde8b9012889ee36757ea
alexjhannan <alexjhannan@gmail.com>
2016-02-09 09:16:33 -0500

    separated out controllers

---
+++ server/controllers/main.js
@@ -0,0 +1,32 @@
+// home page controller
+function indexCtrl(req, res){
+	res.render('main');
+}
+
+// configure smtpTransport for emailer
+var nodemailer = require('nodemailer');
+var smtpTransport = nodemailer.createTransport(process.env.LOGIN);
+
+// contact form post controller
+function contactCtrl(req, res){
+	
+	var automatedMessage = 'This is an automated message from www.alexhannan.com. If you would like to respond to the sender, please write to the email listed below. Do NOT reply to this address.';
+		
+	var mailOptions = {
+		from: req.body.author + '<' + req.body.email + '>',
+		to: 'alexjhannan@gmail.com',
+		subject: req.body.subject,
+		text: automatedMessage + req.body.message + 'From: ' + req.body.author + 'Email:' + req.body.email,
+		html: '<p><strong>' + automatedMessage + '</p></strong>' + '<br /><p>' + req.body.message + '<br /><br />From: ' + req.body.author + '<br />Email: ' + req.body.email +'</p>'
+	}
+	
+	smtpTransport.sendMail(mailOptions, function(err, res){
+		if (err) { console.log(err); }
+		else { console.log('Message sent '); }
+	});
+
+	res.redirect('/');
+}
+
+module.exports.index = indexCtrl;
+module.exports.contact = contactCtrl;
--- server/routes/index.js
+++ server/routes/index.js
@@ -2,34 +2,10 @@ var express = require('express');
 var router = express.Router();
 var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
+var mainCtrl = require('../controllers/main');
 
-router.get('/', function(req, res){
-	res.render('main');
-});
+router.get('/', mainCtrl.index);
 
-// configure smtpTransport for emailer
-var smtpTransport = nodemailer.createTransport(process.env.LOGIN);
-
-// send email with data posted from contact form
-router.post('/contactform', bodyParser.urlencoded({ extended: true }), function(req, res){
-	
-	var automatedMessage = 'This is an automated message from www.alexhannan.com. If you would like to respond to the sender, please write to the email listed below. Do NOT reply to this address.';
-		
-	var mailOptions = {
-		from: req.body.author + '<' + req.body.email + '>',
-		to: 'alexjhannan@gmail.com',
-		subject: req.body.subject,
-		text: automatedMessage + req.body.message + 'From: ' + req.body.author + 'Email:' + req.body.email,
-		html: '<p><strong>' + automatedMessage + '</p></strong>' + '<br /><p>' + req.body.message + '<br /><br />From: ' + req.body.author + '<br />Email: ' + req.body.email +'</p>'
-	}
-	
-	smtpTransport.sendMail(mailOptions, function(err, res){
-		if (err) { console.log(err); }
-		else { console.log('Message sent ');}
-	});
-	
-	res.redirect('/');
-	
-});
+router.post('/contactform', bodyParser.urlencoded({ extended: true }), mainCtrl.contact);
 
 module.exports = router;
